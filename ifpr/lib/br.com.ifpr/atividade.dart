28992dff229546c33c09509a9dfc98c42aa7e5ec
Kenny Fukita <44323619+KennyFukita@users.noreply.github.com>
2021-05-19 23:17:57 -0300

    revisão - variáveis e estrutura de decisão
    revisão - variáveis e estrutura de decisão

---
+++ ifpr/lib/br.com.ifpr/atividade.dart
@@ -0,0 +1,20 @@
+void main(){
+  String nome = "Kenny";
+  int var1 = 10;
+  int var2 = 20;
+  var var3 = 10;
+
+  if (var1 == var3){
+    print("Os valores são iguais");
+  }
+  else{
+    if (var1 > var2){
+      print("Variavel 1 é maior que a variável 2");
+    }
+  }
+}
+
+
+
+
+
